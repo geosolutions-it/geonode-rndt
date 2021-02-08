5dac85e946eadb44646052d16402295ecee327a3
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-02-08 11:49:22 +0100

    Resolve #6: code reformatted with black

--- rndt/__init__.py
+++ rndt/__init__.py
@@ -4,4 +4,3 @@ __author__ = "geosolutions-it"
 __email__ = "info@geosolutionsgroup.com"
 __url__ = "https://github.com/geosolutions-it/geonode-rndt"
 __license__ = "GNU General Public License"
-
--- rndt/admin.py
+++ rndt/admin.py
@@ -3,12 +3,12 @@ from rndt.models import PubblicaAmministrazione, GroupProfileRNDT
 
 # Register your models here.
 
+
 @admin.register(PubblicaAmministrazione)
 class PubblicaAmministrazioneAdmin(admin.ModelAdmin):
-    fields = ('ipa', 'name')
+    fields = ("ipa", "name")
 
 
 @admin.register(GroupProfileRNDT)
 class GroupProfileRNDTAdmin(admin.ModelAdmin):
-    fields = ('group_profile', 'pa')
-
+    fields = ("group_profile", "pa")
