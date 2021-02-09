e4aa525de022f002f557cc7a8949f879b805d13d
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-02-09 12:10:16 +0100

    Resolves #5: Add new test case and improve uuidhandler

--- rndt/tests.py
+++ rndt/tests.py
@@ -6,32 +6,76 @@ import unittest
 
 class TestUUIDHandlerTestCase(unittest.TestCase):
     def setUp(self):
-        self.sut = namedtuple("Instance", ("uuid"))
+        group = namedtuple("Group", ("grouprofile"))
+        groupprofile = namedtuple("GroupProfile", ("groupprofilerndt"))
+        groupprofilerndt = namedtuple("GroupProfileRNDT", ("pa"))
+        pa = namedtuple("PubblicaAmministrazione", ("ipa"))
+        pa.ipa = "c_123"
+        groupprofilerndt.pa = pa
+        groupprofile.groupprofilerndt = groupprofilerndt
+        group.groupprofile = groupprofile
+        self.sut = namedtuple("Instance", ("uuid", "group"))
+        self.sut.group = group
 
-    def test_should_return_an_uuid_if_the_instance_one_is_empty(self):
-        self.sut.uuid = 'ipa:123abc456'
-        actual = UUIDHandler.create_uuid(self.sut)
+    def test_return_an_uuid_if_the_instance_one_is_not_empty(self):
+        self.sut.group.groupprofile.groupprofilerndt.pa.ipa = "ipa"
+        self.sut.uuid = "ipa:123abc456"
+        actual = UUIDHandler(self.sut).create_uuid()
         self.assertEqual("ipa:123abc456", actual)
-    
+
     @patch("uuid.uuid1")
-    def test_should_return_the_instance_uuid_with_the_ipa_code_if_is_not_present(self, mocked_uuid):
+    def test_return_the_instance_uuid_with_the_ipa_code_if_the_uuid_is_not_present(
+        self, mocked_uuid
+    ):
         mocked_uuid.return_value = "123abc456"
-        self.sut.uuid = ''
-        actual = UUIDHandler.create_uuid(self.sut)
-        self.assertEqual("ipa:123abc456", actual)
+        self.sut.uuid = ""
+        actual = UUIDHandler(self.sut).create_uuid()
+        self.assertEqual("c_123:123abc456", actual)
 
-    def test_should_return_the_instance_uuid_with_the_ipa_code_if_ipa_code_is_present(self):
-        self.sut.uuid = 'ipa:123abc456'
-        actual = UUIDHandler.create_uuid(self.sut)
-        self.assertEqual("ipa:123abc456", actual)
+    @patch("uuid.uuid1")
+    def test_return_the_instance_uuid_if_the_uuid_and_ipa_are_not_present(
+        self, mocked_uuid
+    ):
+        mocked_uuid.return_value = "123abc456"
+        self.sut.uuid = ""
+        self.sut.group = None
+        actual = UUIDHandler(self.sut).create_uuid()
+        self.assertEqual("123abc456", actual)
 
-    def test_should_return_the_instance_uuid_with_the_ipa_code_if_ipa_code_is_present(self):
-        self.sut.uuid = 'ipa:123abc456'
-        actual = UUIDHandler.create_uuid(self.sut)
-        self.assertEqual("ipa:123abc456", actual)
+    def test_return_the_instance_uuid_with_the_ipa_code_if_ipa_code_is_present(self):
+        self.sut.group.groupprofile.groupprofilerndt.pa.ipa = "c_345"
+        self.sut.uuid = "c_345:123abc456"
+        actual = UUIDHandler(self.sut).create_uuid()
+        self.assertEqual("c_345:123abc456", actual)
+
+    def test_return_the_instance_uuid_if_the_ipa_code_is_not_found(self):
+        self.sut.group.groupprofile.groupprofilerndt = None
+        self.sut.uuid = "123abc456"
+        actual = UUIDHandler(self.sut).create_uuid()
+        self.assertEqual("123abc456", actual)
 
     def test_the_max_length_of_a_uuid_with_ipa_must_be_36_char(self):
-        self.sut.uuid = 'd1fbf97f-e44a-4c60-87e2-56dc424b29b5-e44a-87e2'
-        actual = UUIDHandler.create_uuid(self.sut)
-        self.assertEqual("ipa:d1fbf97f-e44a-4c60-87e2-56dc424b", actual)
+        self.sut.uuid = "d1fbf97f-e44a-4c60-87e2-56dc424b29b5-e44a-87e2"
+        actual = UUIDHandler(self.sut).create_uuid()
+        self.assertEqual("c_123:d1fbf97f-e44a-4c60-87e2-56dc42", actual)
         self.assertEqual(36, len(actual))
+
+    def test_uuidhandler_should_extract_the_correct_ipa_code_if_exists(self):
+        self.sut.uuid = "c_123:uuid"
+        actual = UUIDHandler(self.sut)._extract_ipa()
+        self.assertEqual("c_123", actual)
+
+    def test_uuidhandler_should_extract_the_correct_uuid_code_if_exists(self):
+        self.sut.uuid = "c_123:uuid"
+        actual = UUIDHandler(self.sut)._extract_uuid()
+        self.assertEqual("uuid", actual)
+
+    def test_uuidhandler_should_extract_none_ipa_code_if_not_exists(self):
+        self.sut.uuid = "d1fbf97f-e44a-4c60-87e2-56dc"
+        actual = UUIDHandler(self.sut)._extract_ipa()
+        self.assertFalse(actual)
+
+    def test_uuidhandler_update_the_ipa_code_if_is_changed(self):
+        self.sut.uuid = "123abc456"
+        actual = UUIDHandler(self.sut).create_uuid()
+        self.assertFalse(actual)
--- rndt/uuidhandler.py
+++ rndt/uuidhandler.py
@@ -1,14 +1,63 @@
+import logging
 import uuid
+import re
 
 
 class UUIDHandler:
-    @staticmethod
-    def create_uuid(instance):
-        if instance.uuid != "":
-            return (
-                f"ipa:{instance.uuid}"[:36]
-                if "ipa" not in instance.uuid
-                else instance.uuid[:36]
-            )
+    def __init__(self, instance):
+        self.instance = instance
+
+    def create_uuid(self):
+        ipa_found = self.evaluate_ipa_code()
+        return self._generate_uuid(ipa_found)
+
+    def _generate_uuid(self, ipa_found):
+        if not self.instance.uuid:
+            return self._uuid_does_not_exists(ipa_found)
+
+        newuuid = ""
+        ipa_available = self._extract_ipa()
+        if ipa_available:
+            if ipa_found == ipa_available:
+                newuuid = self.instance.uuid
+            else:
+                # TODO aggiungere check se l'ipa che è trovato è diverso da quello dell'UUID, è da
+                # mantenere quello nuovo
+                newuuid = f"{ipa_found}:{self.instance.uuid}"
         else:
-            return f"ipa:{uuid.uuid1()}"[:36]
+            if not ipa_found:
+                newuuid = self.instance.uuid
+            elif ipa_found in self.instance.uuid:
+                newuuid = self.instance.uuid
+            else:
+                newuuid = f"{ipa_found}:{self.instance.uuid}"
+        return newuuid[:36]
+
+    def evaluate_ipa_code(self):
+        if self.instance.group is not None:
+            #  gp = group profile
+            try:
+                logging.debug("Retrieving ipa if associated")
+                return self.instance.group.groupprofile.groupprofilerndt.pa.ipa
+            except Exception as e:
+                logging.debug("No ipa found for the selected layer")
+                pass
+        return None
+
+    def _uuid_does_not_exists(self, ipa_found):
+        if not self.instance.uuid and not ipa_found:
+            return f"{uuid.uuid1()}"
+        elif not self.instance.uuid and ipa_found:
+            return f"{ipa_found}:{uuid.uuid1()}"
+
+    def _extract_ipa(self):
+        pattern = re.compile("^\W*(\w+)\W*:")
+        return self.__regex(pattern)
+
+    def _extract_uuid(self):
+        pattern = re.compile(":\W*(\w+)\W*$")
+        return self.__regex(pattern)
+
+    def __regex(self, pattern):
+        match = re.findall(pattern, self.instance.uuid)
+        return match[0] if match else False
