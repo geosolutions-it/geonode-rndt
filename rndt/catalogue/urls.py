b56ce0f6925d4785a6db19b577b9e3e6a0eb9e31
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-03-01 15:52:22 +0100

    Task #7: reduce csw dispatcher overengineering

--- rndt/catalogue/urls.py
+++ rndt/catalogue/urls.py
@@ -3,5 +3,5 @@ from geonode.layers.urls import urlpatterns
 from rndt.catalogue.views import csw_dispatcher
 
 urlpatterns.insert(
-    0, url(r"^csw/(?P<pa_name>[^/]*)/$", csw_dispatcher, name="csw_dispatcher")
+    0, url(r"^csw/(?P<pa_code>[^/]*)/$", csw_dispatcher, name="csw_dispatcher")
 )
--- rndt/catalogue/views.py
+++ rndt/catalogue/views.py
@@ -1,40 +1,12 @@
 
-from django.conf import settings
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http.response import HttpResponse
-from geonode.catalogue.backends.pycsw_local import CONFIGURATION
-from geonode.catalogue.views import CswGlobalDispatcher
-from pycsw import server
+from geonode.catalogue.views import csw_global_dispatch
 
 
-def csw_dispatcher(request, pa_name):    
-    if settings.CATALOGUE['default']['ENGINE'] != 'geonode.catalogue.backends.pycsw_local':
-        return HttpResponseRedirect(settings.CATALOGUE['default']['URL'])
+def csw_dispatcher(request, pa_code):    
+    return csw_global_dispatch(request, LayerFilters(pa_code).filter_layers)
 
-    dispatcher = RndtCswDispatcher(request)
-
-    mdict = dispatcher.get_configurations()
-    env = dispatcher.define_env_data()
-
-    # Save original filter before doing anything
-    mdict_filter = mdict['repository']['filter']
-
-    try:
-        # Filter out Layers not accessible to the User
-        mdict = dispatcher.get_authorized_layer_ids()
-
-        csw = server.Csw(mdict, env, version='2.0.2')
-
-        content = dispatcher.get_xml_tree(csw)
-    finally:
-        # Restore original filter before doing anything
-        mdict['repository']['filter'] = mdict_filter
-
-    return HttpResponse(content, content_type=csw.contenttype)
-
-
-class RndtCswDispatcher(CswGlobalDispatcher):
-    def __init__(self, request):
-        super().__init__(request)
-        # CONFIGURATION['repository']['filter'] = f"uuid like 'ipa5%'"
-        self.mdict = dict(settings.PYCSW['CONFIGURATION'], **CONFIGURATION)
+class LayerFilters():
+    def __init__(self, pa_code):
+        self.pa_code = pa_code
+    def filter_layers(self, layers):
+        return layers.filter(uuid__startswith=self.pa_code)
