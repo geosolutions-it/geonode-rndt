539f764d7e6b818e0efae02d0857d1ecc9f2321c
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-03-01 17:13:38 +0100

    Task #7: reformatting with black

--- rndt/catalogue/views.py
+++ rndt/catalogue/views.py
@@ -1,12 +1,13 @@
-
 from geonode.catalogue.views import csw_global_dispatch
 
 
-def csw_dispatcher(request, pa_code):    
+def csw_dispatcher(request, pa_code):
     return csw_global_dispatch(request, LayerFilters(pa_code).filter_layers)
 
-class LayerFilters():
+
+class LayerFilters:
     def __init__(self, pa_code):
         self.pa_code = pa_code
+
     def filter_layers(self, layers):
         return layers.filter(uuid__startswith=self.pa_code)
