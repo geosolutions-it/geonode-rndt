8b8d0dbdaf7b665bdc4c30e06771666eba2dd228
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-03-04 12:15:56 +0100

    Task #50: merge Add dynamic constraints in UI

--- rndt/context_processors.py
+++ rndt/context_processors.py
@@ -1,5 +1,4 @@
-import ast
-import os
+from django.conf import settings
 
 from rndt.layers.forms import LayerRNDTForm
 
@@ -7,4 +6,5 @@ from rndt.layers.forms import LayerRNDTForm
 def rndt_tags(request):
     return {
         "LayerRNDTForm": LayerRNDTForm,
+        "REQUIRED_RNDT_FIELDS": settings.REQUIRED_RNDT_FIELDS if hasattr(settings, 'REQUIRED_RNDT_FIELDS') else []
     }
