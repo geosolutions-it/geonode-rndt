8244105803b9535e9233298c4e95e99b5727e27a
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-03-04 12:40:59 +0100

    Task 50: removed unused variable

--- rndt/context_processors.py
+++ rndt/context_processors.py
@@ -5,6 +5,5 @@ from rndt.layers.forms import LayerRNDTForm
 
 def rndt_tags(request):
     return {
-        "LayerRNDTForm": LayerRNDTForm,
-        "REQUIRED_RNDT_FIELDS": settings.REQUIRED_RNDT_FIELDS if hasattr(settings, 'REQUIRED_RNDT_FIELDS') else []
+        "LayerRNDTForm": LayerRNDTForm
     }
