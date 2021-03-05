5d3f5b941fc7d4735b1dc0a9e309e61d30ba3e89
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2021-03-05 10:51:04 +0100

    #7 add pycsw endpoints for PAs
    * Task #7: add catalogue
    
    * Task #7: reduce csw dispatcher overengineering
    
    * Task #7: reformatting with black

---
+++ rndt/catalogue/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from geonode.layers.urls import urlpatterns
+from rndt.catalogue.views import csw_dispatcher
+
+urlpatterns.insert(
+    0, url(r"^csw/(?P<pa_code>[^/]*)/$", csw_dispatcher, name="csw_dispatcher")
+)
