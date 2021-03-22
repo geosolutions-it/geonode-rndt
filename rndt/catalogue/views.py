2558f61f4601120813e1cb78e7babfe9faa5f4fe
etj <etj@geo-solutions.it>
2021-03-22 09:53:04 +0100

    #73 CSW getCapabilities URLs for filtered entry points

--- rndt/catalogue/views.py
+++ rndt/catalogue/views.py
@@ -2,7 +2,10 @@ from geonode.catalogue.views import csw_global_dispatch
 
 
 def csw_dispatcher(request, pa_code):
-    return csw_global_dispatch(request, LayerFilters(pa_code).filter_layers)
+    return csw_global_dispatch(
+        request,
+        layer_filter=LayerFilters(pa_code).filter_layers,
+        config_updater=RndtCswConfigurer(pa_code).config_updater)
 
 
 class LayerFilters:
@@ -11,3 +14,13 @@ class LayerFilters:
 
     def filter_layers(self, layers):
         return layers.filter(uuid__startswith=self.pa_code)
+
+
+class RndtCswConfigurer:
+    def __init__(self, pa_code):
+        self.pa_code = pa_code
+
+    def config_updater(self, d: dict) -> dict:
+        old_url = d['server']['url']
+        d['server']['url'] = f'{old_url}/{self.pa_code}'
+        return d
