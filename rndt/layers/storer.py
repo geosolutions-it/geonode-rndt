80da655ecccdc445b9d0332f2afb41c65c86a88d
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-04-15 11:57:43 +0200

    Task #81 rndt storer

--- rndt/layers/storer.py
+++ rndt/layers/storer.py
@@ -3,33 +3,16 @@ from rndt.models import LayerRNDT
 
 def rndt_storer(layer, custom):
 
-    layer, created = LayerRNDT.objects.get_or_create(
+    rndt, created = LayerRNDT.objects.get_or_create(
         layer=layer,
         constraints_other=custom["rndt"].get("constraints_other", None),
         resolution=custom["rndt"].get("resolution", None),
         accuracy=custom["rndt"].get("accuracy", None),
     )
 
+    if not created:
+        rndt.constraints_other=custom["rndt"].get("constraints_other", None)
+        rndt.resolution=custom["rndt"].get("resolution", None)
+        rndt.accuracy=custom["rndt"].get("accuracy", None)
+        rndt.save()
     return layer
-
-
-class RNDTStorer:
-    def __init__(self, layer, custom):
-        self.layer = layer
-        self.custom = custom
-
-    def create(self):
-
-        pass
-
-    def update(self):
-        pass
-
-    def layer_rndt_exists(self):
-        if self.layer:
-            rndt = LayerRNDT.objects.filter(layer=self.layer)
-            return rndt.exists()
-        return False
-
-    def _generate_json(self):
-        return {""}
