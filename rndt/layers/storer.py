f0f50f74f82cbc68c7fd836fe11832b60eefeb4d
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-04-15 16:25:19 +0200

    Task #81 Check fi the dict is not None

--- rndt/layers/storer.py
+++ rndt/layers/storer.py
@@ -2,17 +2,18 @@ from rndt.models import LayerRNDT
 
 
 def rndt_storer(layer, custom):
+    rndt_dict = custom.get('rndt', None)
+    if rndt_dict is not None:
+        rndt, created = LayerRNDT.objects.get_or_create(
+            layer=layer,
+            constraints_other=rndt_dict.get("constraints_other", None),
+            resolution=rndt_dict.get("resolution", None),
+            accuracy=rndt_dict.get("accuracy", None),
+        )
 
-    rndt, created = LayerRNDT.objects.get_or_create(
-        layer=layer,
-        constraints_other=custom["rndt"].get("constraints_other", None),
-        resolution=custom["rndt"].get("resolution", None),
-        accuracy=custom["rndt"].get("accuracy", None),
-    )
-
-    if not created:
-        rndt.constraints_other=custom["rndt"].get("constraints_other", None)
-        rndt.resolution=custom["rndt"].get("resolution", None)
-        rndt.accuracy=custom["rndt"].get("accuracy", None)
-        rndt.save()
+        if not created:
+            rndt.constraints_other=rndt_dict.get("constraints_other", None)
+            rndt.resolution=rndt_dict.get("resolution", None)
+            rndt.accuracy=rndt_dict.get("accuracy", None)
+            rndt.save()
     return layer
