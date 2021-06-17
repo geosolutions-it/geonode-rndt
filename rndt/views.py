39b9d99acebb4de44a09434b20bc33a74124ade2
mattiagiupponi <mattia.giupponi@gmail.com>
2021-06-17 12:08:43 +0200

    [Almaviva #260] Fix PA count by filtering by resources and show PA facet only for layers

--- rndt/views.py
+++ rndt/views.py
@@ -2,10 +2,10 @@ import json
 import time
 
 from django.core.serializers.json import DjangoJSONEncoder
-from geonode.api.api import TypeFilteredResource
-from geonode.api.authorization import ApiLockdownAuthorization
-from geonode.layers.models import Layer
-from tastypie.constants import ALL
+from geonode.api.api import  TypeFilteredResource
+from geonode.api.authorization import (ApiLockdownAuthorization,)
+from geonode.base.models import ResourceBase
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie.serializers import Serializer
 
 from rndt.models import PubblicaAmministrazione
@@ -16,8 +16,10 @@ class PACountJSONSerializer(Serializer):
     def get_resources_counts(self, options):
         counts = dict()
         for pa in PubblicaAmministrazione.objects.all():
-            layer_with_ipa = Layer.objects.filter(uuid__startswith=pa.ipa).count()
-            counts[pa.id] = layer_with_ipa
+            resources = ResourceBase.objects.filter(group__groupprofile__groupprofilerndt__pa__ipa=pa.ipa)
+            if options.get('resource_type_filter', None) is not None:
+                resources = resources.filter(resource_type=options.get('resource_type_filter'))
+            counts[pa.id] = resources.count()
         return counts
 
     def to_json(self, data, options=None):
@@ -31,6 +33,9 @@ class PACountJSONSerializer(Serializer):
         data['requested_time'] = time.time()
 
         return json.dumps(data, cls=DjangoJSONEncoder, sort_keys=True)
+
+
+
 class PubblicaAmministrazioneResource(TypeFilteredResource):
     """PA api"""
 
@@ -39,6 +44,9 @@ class PubblicaAmministrazioneResource(TypeFilteredResource):
             options = {}
         options['count_type'] = 'id'
 
+        if request.GET.get('type', None) is not None:
+            options['resource_type_filter'] = request.GET.get('type')
+
         return super(PubblicaAmministrazioneResource, self).serialize(request, data, format, options)
 
     class Meta:
@@ -46,7 +54,7 @@ class PubblicaAmministrazioneResource(TypeFilteredResource):
         resource_name = 'pubblica_amministrazione'
         allowed_methods = ['get']
         filtering = {
-            'ipa': ALL,
+            'ipa': ALL_WITH_RELATIONS,
             'name': ALL
         }
         serializer = PACountJSONSerializer()
